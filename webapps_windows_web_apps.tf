8abf6e617ed022a870354c03038e1a338ef086eb
rfernandezdo <rafael_fernandez@outlook.com>
2025-03-08 16:02:40 +0100

    feat(webapps): add application insights to remote_objects in Windows Web Apps module

--- webapps_windows_web_apps.tf
+++ webapps_windows_web_apps.tf
@@ -1,7 +1,7 @@
 module "windows_web_apps" {
-  source   = "./modules/webapps/windows_web_app"
-  depends_on        = [module.service_plans, module.networking]
-  for_each = local.webapp.windows_web_apps
+  source     = "./modules/webapps/windows_web_app"
+  depends_on = [module.service_plans, module.networking]
+  for_each   = local.webapp.windows_web_apps
 
   client_config     = local.client_config
   global_settings   = local.global_settings
@@ -12,16 +12,17 @@ module "windows_web_apps" {
   settings          = each.value
 
   remote_objects = {
-    app_service_plans   = local.combined_objects_app_service_plans
-    service_plans       = local.combined_objects_service_plans
-    combined_objects    = local.dynamic_app_settings_combined_objects
-    diagnostics         = local.combined_diagnostics
-    keyvaults           = local.combined_objects_keyvaults
-    storage_accounts    = local.combined_objects_storage_accounts
-    vnets               = local.combined_objects_networking
-    virtual_subnets     = local.combined_objects_virtual_subnets
-    managed_identities  = local.combined_objects_managed_identities
-    private_dns         = local.combined_objects_private_dns
+    app_service_plans    = local.combined_objects_app_service_plans
+    service_plans        = local.combined_objects_service_plans
+    combined_objects     = local.dynamic_app_settings_combined_objects
+    diagnostics          = local.combined_diagnostics
+    keyvaults            = local.combined_objects_keyvaults
+    storage_accounts     = local.combined_objects_storage_accounts
+    vnets                = local.combined_objects_networking
+    virtual_subnets      = local.combined_objects_virtual_subnets
+    managed_identities   = local.combined_objects_managed_identities
+    private_dns          = local.combined_objects_private_dns
+    application_insights = local.combined_objects_application_insights
   }
 }
 
