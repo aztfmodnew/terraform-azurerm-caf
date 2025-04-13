25a2490e74c162163086dd48e229ad333a85e396
rfernandezdo <rafael_fernandez@outlook.com>
2025-04-13 21:42:10 +0200

    fix(azuread): update for_each conditions for group member resources

--- modules/azuread/groups_members/membership/membership.tf
+++ modules/azuread/groups_members/membership/membership.tf
@@ -1,26 +1,30 @@
 resource "azuread_group_member" "group_ids" {
-  for_each = var.azuread_groups != {} && can(var.members.keys) ? { for k, v in var.members : k => v if contains(keys(var.azuread_groups), k) } : {}
+  #for_each = var.azuread_groups != {} ? toset(try(var.members.keys, [])) : []
+  for_each = length(try(var.azuread_groups,[])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.azuread_groups[each.key].object_id
 }
 
 resource "azuread_group_member" "ids" {
-  for_each = var.azuread_service_principals != {} && can(var.members.keys) ? { for k, v in var.members : k => v if contains(keys(var.azuread_service_principals), k) } : {}
+  #for_each = var.azuread_service_principals != {} ? toset(try(var.members.keys, [])) : []
+  for_each = length(try(var.azuread_service_principals,[])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.azuread_service_principals[each.key].object_id
 }
 
 resource "azuread_group_member" "msi_ids" {
-  for_each = var.managed_identities != {} && can(var.members.keys) ? { for k, v in var.members : k => v if contains(keys(var.managed_identities), k) } : {}
+  #for_each = var.managed_identities != {} ? toset(try(var.members.keys, [])) : []
+  for_each = length(try(var.managed_identities,[])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.managed_identities[each.key].principal_id
 }
 
 resource "azuread_group_member" "mssql_server_ids" {
-  for_each = var.mssql_servers != {} && can(var.members.keys) ? { for k, v in var.members : k => v if contains(keys(var.mssql_servers), k) } : {}
+# for_each = var.mssql_servers != {} ? toset(try(var.members.keys, [])) : []
+  for_each = length(try(var.mssql_servers,[])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.mssql_servers[each.key].rbac_id
