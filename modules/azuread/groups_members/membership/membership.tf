d85802cd3b622edc345b61a65c4f2a15ce3ac6ef
rfernandezdo <rafael_fernandez@outlook.com>
2025-04-15 13:30:10 +0200

    fix(azuread): simplify for_each conditions by removing unnecessary try calls

--- modules/azuread/groups_members/membership/membership.tf
+++ modules/azuread/groups_members/membership/membership.tf
@@ -1,26 +1,26 @@
 resource "azuread_group_member" "group_ids" {
-  for_each = length(try(var.azuread_groups, [])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
+  for_each = length(var.azuread_groups) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.azuread_groups[each.key].object_id
 }
 
 resource "azuread_group_member" "ids" {
-  for_each = length(try(var.azuread_service_principals, [])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
+  for_each = length(var.azuread_service_principals) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.azuread_service_principals[each.key].object_id
 }
 
 resource "azuread_group_member" "msi_ids" {
-  for_each = length(try(var.managed_identities, [])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
+  for_each = length(var.managed_identities) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.managed_identities[each.key].principal_id
 }
 
 resource "azuread_group_member" "mssql_server_ids" {
-  for_each = length(try(var.mssql_servers, [])) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
+  for_each = length(var.mssql_servers) > 0 && length(try(var.members.keys, [])) > 0 ? toset(try(var.members.keys, [])) : []
 
   group_object_id  = var.group_object_id
   member_object_id = var.mssql_servers[each.key].rbac_id
