91446e3381936e11992d43be2569b118e5bc966b
rfernandezdo <39990341+rfernandezdo@users.noreply.github.com>
2025-12-26 20:22:55 +0000

    Apply terraform fmt

--- cache_managed_redis.tf
+++ cache_managed_redis.tf
@@ -2,9 +2,9 @@ module "managed_redis" {
   source   = "./modules/cache/managed_redis"
   for_each = try(local.cache.managed_redis, {})
 
-  global_settings     = local.global_settings
-  client_config       = local.client_config
-  settings            = each.value
+  global_settings = local.global_settings
+  client_config   = local.client_config
+  settings        = each.value
   location = try(
     each.value.location,
     local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
@@ -13,8 +13,8 @@ module "managed_redis" {
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   remote_objects = {
-    diagnostics          = local.combined_diagnostics
-    managed_identities   = local.combined_objects_managed_identities
+    diagnostics        = local.combined_diagnostics
+    managed_identities = local.combined_objects_managed_identities
   }
   diagnostic_profiles = try(each.value.diagnostic_profiles, {})
   diagnostics         = local.combined_diagnostics
