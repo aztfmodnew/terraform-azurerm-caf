8c66a490205770ed32be0c07e81da79189b35d35
rfernandezdo <39990341+rfernandezdo@users.noreply.github.com>
2026-03-06 17:16:16 +0000

    Apply terraform fmt

--- modules/databases/mssql_elastic_pool/elastic_pool.tf
+++ modules/databases/mssql_elastic_pool/elastic_pool.tf
@@ -12,13 +12,13 @@ locals {
 
 resource "azurecaf_name" "elasticpool" {
 
-  name          = var.settings.name
-  resource_type = "azurerm_mssql_elasticpool"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
-  clean_input   = true
-  passthrough   = var.global_settings.passthrough
-  use_slug      = var.global_settings.use_slug
+  name            = var.settings.name
+  resource_type   = "azurerm_mssql_elasticpool"
+  prefixes        = var.global_settings.prefixes
+  random_length   = var.global_settings.random_length
+  clean_input     = true
+  passthrough     = var.global_settings.passthrough
+  use_slug        = var.global_settings.use_slug
   use_legacy_slug = local.use_legacy_slug
 }
 
