ceab2d8776c9b9f0ab57fb282a8ad97bb8807769
Rafael Fernández <39990341+rfernandezdo@users.noreply.github.com>
2026-06-23 16:52:00 +0200

    Update backup_vault_policy.tf

--- modules/backup_vault/backup_vault_policy_blob_storage/backup_vault_policy.tf
+++ modules/backup_vault/backup_vault_policy_blob_storage/backup_vault_policy.tf
@@ -9,7 +9,36 @@ resource "azurecaf_name" "backup_vault_policy" {
 }
 
 resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
-  name                                   = azurecaf_name.backup_vault_policy.result
-  vault_id                               = var.vault_id
-  operational_default_retention_duration = var.settings.operational_default_retention_duration
+  name     = azurecaf_name.backup_vault_policy.result
+  vault_id = var.vault_id
+
+  # OperationalStore: retención máx 360 días. Mutuamente excluyente con vault_default_retention_duration.
+  operational_default_retention_duration = lookup(var.settings, "operational_default_retention_duration", null)
+
+  # VaultStore: vault_default_retention_duration requiere backup_repeating_time_intervals.
+  vault_default_retention_duration    = lookup(var.settings, "vault_default_retention_duration", null)
+  backup_repeating_time_intervals     = lookup(var.settings, "backup_repeating_time_intervals", null)
+  time_zone                           = lookup(var.settings, "time_zone", null)
+
+  dynamic "retention_rule" {
+    for_each = lookup(var.settings, "retention_rules", [])
+    content {
+      name     = retention_rule.value.name
+      priority = retention_rule.value.priority
+
+      life_cycle {
+        duration        = retention_rule.value.life_cycle.duration
+        data_store_type = retention_rule.value.life_cycle.data_store_type
+      }
+
+      criteria {
+        absolute_criteria      = lookup(retention_rule.value.criteria, "absolute_criteria", null)
+        days_of_week           = lookup(retention_rule.value.criteria, "days_of_week", null)
+        days_of_month          = lookup(retention_rule.value.criteria, "days_of_month", null)
+        months_of_year         = lookup(retention_rule.value.criteria, "months_of_year", null)
+        weeks_of_month         = lookup(retention_rule.value.criteria, "weeks_of_month", null)
+        scheduled_backup_times = lookup(retention_rule.value.criteria, "scheduled_backup_times", null)
+      }
+    }
+  }
 }
