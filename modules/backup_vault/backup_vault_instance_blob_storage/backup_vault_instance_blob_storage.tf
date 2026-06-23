3763eaeff49f1dd2e28f0cea067439d34b00dd4f
Rafael Fernández <39990341+rfernandezdo@users.noreply.github.com>
2026-06-23 16:13:24 +0200

    Update backup_vault_instance_blob_storage.tf

--- modules/backup_vault/backup_vault_instance_blob_storage/backup_vault_instance_blob_storage.tf
+++ modules/backup_vault/backup_vault_instance_blob_storage/backup_vault_instance_blob_storage.tf
@@ -4,4 +4,8 @@ resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_in
   location           = var.location
   storage_account_id = var.storage_account_id
   backup_policy_id   = var.backup_policy_id
+
+  # Required for VaultStore: list of containers to protect. 
+  # If omitted Azure returns UserErrorInvalidRequestParameter (BackupDatasourceParameters cannot be null).
+  storage_account_container_names = lookup(var.settings, "storage_account_container_names", null)
 }
